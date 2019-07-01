829e097a80d9335ea76d82f3eb6d3f8422e026a4
M. Imam Pratama <imambungo@gmail.com>
2019-07-01 20:07:30 +0700

    first problem

---
+++ 1_lifeUniverseEverything.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int input;
+	while (true)
+	{
+		scanf("%d", &input);
+
+		if (input != 42)
+			printf("%d\n", input);
+		else
+			break;
+	}
+}
